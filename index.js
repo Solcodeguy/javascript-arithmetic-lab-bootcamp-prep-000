d172c0baed0d5603da9371630773449937ca748b
shawn a meighn <smeighn@gmail.com>
2018-12-31 22:49:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+let number;
+function add(a, b){ //add a and b and return the result.
+  number = a + b;
+  return number;
+}
+function subtract(a, b){ //subtract a and b and return the result.
+  number = a - b;
+  return number;
+}
+function multiply(a, b){ //multiply a and b and return the result.
+  number = a * b;
+  return number;
+}
