9592e11c77913cd3028ee9ab2a701dc05d6fead3
shawn a meighn <smeighn@gmail.com>
2018-12-31 22:52:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ function multiply(a, b){
   number = a * b;
   return number;
 }
+function divide(a, b){ //divide a and b and return the result.
+  number = a / b;
+  return number;
+}
+function inc(a){ //Increment a by 1 and return the result.
+  number = a + 1;
+  return number;
+}
+function dec(a){ //Decrement a by 1 and return the result.
+  number = a - 1;
+  return number;
+}
+function makeInt(a){ //Parse a as an integer and return that integer.
+  number = parseInt(a, 10);
+  return number;
+}
+function preserveDecimal(a){ //Parse a as a float (number with decimals) and return that number.
+  number = parseFloat(a, 10);
+  return number;
+}
